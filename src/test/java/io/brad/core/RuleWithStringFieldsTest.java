c3a212e504c04def9693b417e1f0b3118996c449
Emmanuel Soyer <emmanuel.soyer1@gmail.com>
2022-04-22 21:43:52 +0200

    Write an utility class which allows model creation at runtime in order to simplify tests writing

--- src/test/java/io/brad/core/RuleWithStringFieldsTest.java
+++ src/test/java/io/brad/core/RuleWithStringFieldsTest.java
@@ -1,177 +1,176 @@
 package io.brad.core;
 
+import io.brad.core.fields.StringField;
 import io.brad.core.rules.Rule;
-import io.brad.sample.Pojo;
-import io.brad.sample.PojoFields;
 import org.junit.jupiter.api.Test;
 
 import static io.brad.assertions.RuleAssertion.assertThat;
-import static io.brad.sample.PojoFields.field1;
-import static io.brad.sample.PojoFields.field2;
 
 public class RuleWithStringFieldsTest {
 
-    private static final Rule<Pojo> field1_eq_value = field1.eq("value");
-    private static final Rule<Pojo> field1_eq_field2 = field1.eq(field2);
-    private static final Rule<Pojo> field1_not_eq_value = field1.notEq("value");
-    private static final Rule<Pojo> field1_not_eq_field2 = field1.notEq(field2);
-    private static final Rule<Pojo> field1_is_null = field1.isNull();
-    private static final Rule<Pojo> field1_is_not_null = field1.isNotNull();
-    private static final Rule<Pojo> field1_in_value1_value2 = field1.in("value1", "value2");
-    private static final Rule<Pojo> field1_not_in_value1_value2 = field1.notIn("value1", "value2");
-    private static final Rule<Pojo> field1_contains_value = field1.contains("value");
-    private static final Rule<Pojo> field1_does_not_contain_value = field1.doesNotContain("value");
-    private static final Rule<Pojo> field1_contains_field2 = field1.contains(field2);
-    private static final Rule<Pojo> field1_does_not_contain_field2 = field1.doesNotContain(field2);
-    private static final Rule<Pojo> field1_starts_with_value = field1.startsWith("value");
-    private static final Rule<Pojo> field1_does_not_start_with_value = field1.doesNotStartWith("value");
-    private static final Rule<Pojo> field1_starts_with_field2 = field1.startsWith(field2);
-    private static final Rule<Pojo> field1_does_not_start_with_field2 = field1.doesNotStartWith(field2);
-    private static final Rule<Pojo> field1_ends_with_value = field1.endsWith("value");
-    private static final Rule<Pojo> field1_does_not_end_with_value = field1.doesNotEndWith("value");
-    private static final Rule<Pojo> field1_ends_with_field2 = field1.endsWith(field2);
-    private static final Rule<Pojo> field1_does_not_end_with_field2 = field1.doesNotEndWith(field2);
+    private final RuntimeModel model = new RuntimeModel();
 
-    private final Pojo pojo = new Pojo();
-    private final ModelFields<Pojo> pojoFields = new PojoFields();
+    private final StringField<RuntimeModel> field1 = model.addStringField("field1", "value");
+    private final StringField<RuntimeModel> field2 = model.addStringField("field2", "value");
+
+    private final Rule<RuntimeModel> field1_eq_value = field1.eq("value");
+    private final Rule<RuntimeModel> field1_eq_field2 = field1.eq(field2);
+    private final Rule<RuntimeModel> field1_not_eq_value = field1.notEq("value");
+    private final Rule<RuntimeModel> field1_not_eq_field2 = field1.notEq(field2);
+    private final Rule<RuntimeModel> field1_is_null = field1.isNull();
+    private final Rule<RuntimeModel> field1_is_not_null = field1.isNotNull();
+    private final Rule<RuntimeModel> field1_in_value1_value2 = field1.in("value1", "value2");
+    private final Rule<RuntimeModel> field1_not_in_value1_value2 = field1.notIn("value1", "value2");
+    private final Rule<RuntimeModel> field1_contains_value = field1.contains("value");
+    private final Rule<RuntimeModel> field1_does_not_contain_value = field1.doesNotContain("value");
+    private final Rule<RuntimeModel> field1_contains_field2 = field1.contains(field2);
+    private final Rule<RuntimeModel> field1_does_not_contain_field2 = field1.doesNotContain(field2);
+    private final Rule<RuntimeModel> field1_starts_with_value = field1.startsWith("value");
+    private final Rule<RuntimeModel> field1_does_not_start_with_value = field1.doesNotStartWith("value");
+    private final Rule<RuntimeModel> field1_starts_with_field2 = field1.startsWith(field2);
+    private final Rule<RuntimeModel> field1_does_not_start_with_field2 = field1.doesNotStartWith(field2);
+    private final Rule<RuntimeModel> field1_ends_with_value = field1.endsWith("value");
+    private final Rule<RuntimeModel> field1_does_not_end_with_value = field1.doesNotEndWith("value");
+    private final Rule<RuntimeModel> field1_ends_with_field2 = field1.endsWith(field2);
+    private final Rule<RuntimeModel> field1_does_not_end_with_field2 = field1.doesNotEndWith(field2);
 
     @Test
     void field1_eq_value() {
-        assertThat(field1_eq_value, pojoFields)
-                .validates(pojo.setField1("value"))
-                .doesNotValidate(pojo.setField1("not_value"));
+        assertThat(field1_eq_value, model)
+                .validates(model.set(field1, "value"))
+                .doesNotValidate(model.set(field1, "not_value"));
     }
 
     @Test
     void field1_not_eq_value() {
-        assertThat(field1_not_eq_value, pojoFields)
-                .validates(pojo.setField1("not_value"))
-                .doesNotValidate(pojo.setField1("value"));
+        assertThat(field1_not_eq_value, model)
+                .validates(model.set(field1, "not_value"))
+                .doesNotValidate(model.set(field1, "value"));
     }
 
     @Test
     void field1_eq_field2() {
-        assertThat(field1_eq_field2, pojoFields)
-                .validates(pojo.setField1("value").setField2("value"))
-                .doesNotValidate(pojo.setField1("value1").setField2("value2"));
+        assertThat(field1_eq_field2, model)
+                .validates(model.set(field1, "value").set(field2, "value"))
+                .doesNotValidate(model.set(field1, "value1").set(field2, "value2"));
     }
 
     @Test
     void field1_not_eq_field2() {
-        assertThat(field1_not_eq_field2, pojoFields)
-                .validates(pojo.setField1("value").setField2("not_value"))
-                .doesNotValidate(pojo.setField1("value").setField2("value"));
+        assertThat(field1_not_eq_field2, model)
+                .validates(model.set(field1, "value").set(field2, "not_value"))
+                .doesNotValidate(model.set(field1, "value").set(field2, "value"));
     }
 
     @Test
     void field1_is_null() {
-        assertThat(field1_is_null, pojoFields)
-                .validates(pojo.setField1(null))
-                .doesNotValidate(pojo.setField1("value"));
+        assertThat(field1_is_null, model)
+                .validates(model.set(field1, null))
+                .doesNotValidate(model.set(field1, "value"));
     }
 
     @Test
     void field1_is_not_null() {
-        assertThat(field1_is_not_null, pojoFields)
-                .validates(pojo.setField1("value"))
-                .doesNotValidate(pojo.setField1(null));
+        assertThat(field1_is_not_null, model)
+                .validates(model.set(field1, "value"))
+                .doesNotValidate(model.set(field1, null));
     }
 
     @Test
     void field1_in_value1_value2() {
-        assertThat(field1_in_value1_value2, pojoFields)
-                .validates(pojo.setField1("value1"))
-                .doesNotValidate(pojo.setField1("value3"));
+        assertThat(field1_in_value1_value2, model)
+                .validates(model.set(field1, "value1"))
+                .doesNotValidate(model.set(field1, "value3"));
     }
 
     @Test
     void field1_not_in_value1_value2() {
-        assertThat(field1_not_in_value1_value2, pojoFields)
-                .validates(pojo.setField1("value3"))
-                .doesNotValidate(pojo.setField1("value1"));
+        assertThat(field1_not_in_value1_value2, model)
+                .validates(model.set(field1, "value3"))
+                .doesNotValidate(model.set(field1, "value1"));
     }
 
     @Test
     void field1_contains_value() {
-        assertThat(field1_contains_value, pojoFields)
-                .validates(pojo.setField1("contains_value"))
-                .doesNotValidate(pojo.setField1("doesnt"));
+        assertThat(field1_contains_value, model)
+                .validates(model.set(field1, "contains_value"))
+                .doesNotValidate(model.set(field1, "doesnt"));
     }
 
     @Test
     void field1_does_not_contain_value() {
-        assertThat(field1_does_not_contain_value, pojoFields)
-                .validates(pojo.setField1("doesnt"))
-                .doesNotValidate(pojo.setField1("contains_value"));
+        assertThat(field1_does_not_contain_value, model)
+                .validates(model.set(field1, "doesnt"))
+                .doesNotValidate(model.set(field1, "contains_value"));
     }
 
     @Test
     void field1_contains_field2() {
-        assertThat(field1_contains_field2, pojoFields)
-                .validates(pojo.setField1("contains_value").setField2("value"))
-                .doesNotValidate(pojo.setField1("doesnt").setField2("value"));
+        assertThat(field1_contains_field2, model)
+                .validates(model.set(field1, "contains_value").set(field2, "value"))
+                .doesNotValidate(model.set(field1, "doesnt").set(field2, "value"));
     }
 
     @Test
     void field1_does_not_contain_field2() {
-        assertThat(field1_does_not_contain_field2, pojoFields)
-                .validates(pojo.setField1("doesnt").setField2("value"))
-                .doesNotValidate(pojo.setField1("contains_value").setField2("value"));
+        assertThat(field1_does_not_contain_field2, model)
+                .validates(model.set(field1, "doesnt").set(field2, "value"))
+                .doesNotValidate(model.set(field1, "contains_value").set(field2, "value"));
     }
 
     @Test
     void field1_starts_with_value() {
-        assertThat(field1_starts_with_value, pojoFields)
-                .validates(pojo.setField1("value_blabla"))
-                .doesNotValidate(pojo.setField1("blabla_value"));
+        assertThat(field1_starts_with_value, model)
+                .validates(model.set(field1, "value_blabla"))
+                .doesNotValidate(model.set(field1, "blabla_value"));
     }
 
     @Test
     void field1_does_not_start_with_value() {
-        assertThat(field1_does_not_start_with_value, pojoFields)
-                .validates(pojo.setField1("blabla_value"))
-                .doesNotValidate(pojo.setField1("value_blabla"));
+        assertThat(field1_does_not_start_with_value, model)
+                .validates(model.set(field1, "blabla_value"))
+                .doesNotValidate(model.set(field1, "value_blabla"));
     }
 
     @Test
     void field1_starts_with_field2() {
-        assertThat(field1_starts_with_field2, pojoFields)
-                .validates(pojo.setField1("value_blabla").setField2("value"))
-                .doesNotValidate(pojo.setField1("blabla_value").setField2("value"));
+        assertThat(field1_starts_with_field2, model)
+                .validates(model.set(field1, "value_blabla").set(field2, "value"))
+                .doesNotValidate(model.set(field1, "blabla_value").set(field2, "value"));
     }
 
     @Test
     void field1_does_not_start_with_field2() {
-        assertThat(field1_does_not_start_with_field2, pojoFields)
-                .validates(pojo.setField1("blabla_value").setField2("value"))
-                .doesNotValidate(pojo.setField1("value_blabla").setField2("value"));
+        assertThat(field1_does_not_start_with_field2, model)
+                .validates(model.set(field1, "blabla_value").set(field2, "value"))
+                .doesNotValidate(model.set(field1, "value_blabla").set(field2, "value"));
     }
 
     @Test
     void field1_ends_with_value() {
-        assertThat(field1_ends_with_value, pojoFields)
-                .validates(pojo.setField1("blabla_value"))
-                .doesNotValidate(pojo.setField1("value_blabla"));
+        assertThat(field1_ends_with_value, model)
+                .validates(model.set(field1, "blabla_value"))
+                .doesNotValidate(model.set(field1, "value_blabla"));
     }
 
     @Test
     void field1_does_not_end_with_value() {
-        assertThat(field1_does_not_end_with_value, pojoFields)
-                .validates(pojo.setField1("value_blabla"))
-                .doesNotValidate(pojo.setField1("blabla_value"));
+        assertThat(field1_does_not_end_with_value, model)
+                .validates(model.set(field1, "value_blabla"))
+                .doesNotValidate(model.set(field1, "blabla_value"));
     }
 
     @Test
     void field1_ends_with_field2() {
-        assertThat(field1_ends_with_field2, pojoFields)
-                .validates(pojo.setField1("blabla_value").setField2("value"))
-                .doesNotValidate(pojo.setField1("value_blabla").setField2("value"));
+        assertThat(field1_ends_with_field2, model)
+                .validates(model.set(field1, "blabla_value").set(field2, "value"))
+                .doesNotValidate(model.set(field1, "value_blabla").set(field2, "value"));
     }
 
     @Test
     void field1_does_not_end_with_field2() {
-        assertThat(field1_does_not_end_with_field2, pojoFields)
-                .validates(pojo.setField1("value_blabla").setField2("value"))
-                .doesNotValidate(pojo.setField1("blabla_value").setField2("value"));
+        assertThat(field1_does_not_end_with_field2, model)
+                .validates(model.set(field1, "value_blabla").set(field2, "value"))
+                .doesNotValidate(model.set(field1, "blabla_value").set(field2, "value"));
     }
 }
---
+++ src/test/java/io/brad/core/RuntimeModel.java
@@ -0,0 +1,82 @@
+package io.brad.core;
+
+import io.brad.core.fields.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class RuntimeModel implements ModelFields<RuntimeModel> {
+
+    private final Map<String, Object> values;
+    private final Map<String, FieldFromModel<RuntimeModel, ?>> fields;
+
+    public RuntimeModel() {
+        values = new HashMap<>();
+        fields = new HashMap<>();
+    }
+
+    public <T> RuntimeModel set(FieldFromModel<RuntimeModel, T> field, T value) {
+        if (!fields.containsValue(field)) {
+            throw new IllegalArgumentException(field + " does not exist in model");
+        }
+        values.put(field.getCode(), value);
+        return this;
+    }
+
+    public StringField<RuntimeModel> addStringField(String fieldName, String value) {
+        return addField(fieldName, value, StringField::new);
+    }
+
+    public IntegerField<RuntimeModel> addIntegerField(String fieldName, Integer value) {
+        return addField(fieldName, value, IntegerField::new);
+    }
+
+    public LongField<RuntimeModel> addLongField(String fieldName, Long value) {
+        return addField(fieldName, value, LongField::new);
+    }
+
+    public FloatField<RuntimeModel> addFloatField(String fieldName, Float value) {
+        return addField(fieldName, value, FloatField::new);
+    }
+
+    public DoubleField<RuntimeModel> addDoubleField(String fieldName, Double value) {
+        return addField(fieldName, value, DoubleField::new);
+    }
+
+    public BooleanField<RuntimeModel> addBooleanField(String fieldName, Boolean value) {
+        return addField(fieldName, value, BooleanField::new);
+    }
+
+    public LocalDateField<RuntimeModel> addLocalDateField(String fieldName, LocalDate value) {
+        return addField(fieldName, value, LocalDateField::new);
+    }
+
+    public LocalDateTimeField<RuntimeModel> addLocalDateTimeField(String fieldName, LocalDateTime value) {
+        return addField(fieldName, value, LocalDateTimeField::new);
+    }
+
+    private <T, V extends FieldFromModel<RuntimeModel, T>> V addField(String fieldName,
+                                                                      T value,
+                                                                      BiFunction<String, Function<RuntimeModel, T>, V> fieldCreator) {
+        if (fields.containsKey(fieldName)) {
+            throw new IllegalArgumentException("Model contains already field " + fieldName);
+        }
+
+        @SuppressWarnings("unchecked")
+        Function<RuntimeModel, T> propertyAccessor = r -> (T) values.get(fieldName);
+        V field = fieldCreator.apply(fieldName, propertyAccessor);
+        values.put(fieldName, value);
+        fields.put(fieldName, field);
+        return field;
+    }
+
+    @Override
+    public List<? extends FieldFromModel<RuntimeModel, ?>> getFields() {
+        return fields.values().stream().toList();
+    }
+}
